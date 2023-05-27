f715d1c15e473a7e92c1c4ef13624ccea5acf227
D-Tarun-1 <119721564+D-Tarun-1@users.noreply.github.com>
2023-05-27 21:51:40 +0530

    import java.util.Scanner; public class GCDOfTwoNumbers {    public static void main(String args[]){       int a, b, i, hcf = 0;       Scanner sc = new Scanner(System.in);       a = sc.nextInt();       b = sc.nextInt();
          for(i = 1; i <= a || i <= b; i++) {
             if( a%i == 0 && b%i == 0 )
             hcf = i;
          }
          System.out.println(hcf);
       }
    }

---
+++ GCD_or_HCF.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class GCDOfTwoNumbers {
+   public static void main(String args[]){
+      int a, b, i, hcf = 0;
+      Scanner sc = new Scanner(System.in);
+      a = sc.nextInt();
+      b = sc.nextInt();
+
+      for(i = 1; i <= a || i <= b; i++) {
+         if( a%i == 0 && b%i == 0 )
+         hcf = i;
+      }
+      System.out.println(hcf);
+   }
+}
