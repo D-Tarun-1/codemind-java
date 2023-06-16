e84a6483c94d3f64c1fdd5bb874ca413a6e9de2a
D-Tarun-1 <119721564+D-Tarun-1@users.noreply.github.com>
2023-06-16 20:16:24 +0530

    import java.util.*; public class prime {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,m,i,j,s=0,c;         n=sc.nextInt();         m=sc.nextInt();         for(i=n;i<=m;i++)         {             c=1;             for(j=2;j<=Math.sqrt(i);j++)             {                 if(i%j==0)                 {                     c=0;                     break;                 }             }             if(c==1&&i!=1)             {                 s++;             }         }         System.out.println(s);     } }

--- Primes_in_range.java
+++ Primes_in_range.java
@@ -4,26 +4,25 @@ public class prime
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int i,j,c,s=0,n,m;
+        int n,m,i,j,s=0,c;
         n=sc.nextInt();
         m=sc.nextInt();
         for(i=n;i<=m;i++)
-         {
+        {
             c=1;
-           for(j=2;j<=Math.sqrt(i);j++)
-           {
-               if(i%j==0)
-               {
-                  c=0;
-                  break;
-               }
-          }
-                if(c==1&&i!=1)
-                 {
-                   s++;
-                   }
-         }
-       System.out.println(s);
-    
+            for(j=2;j<=Math.sqrt(i);j++)
+            {
+                if(i%j==0)
+                {
+                    c=0;
+                    break;
+                }
+            }
+            if(c==1&&i!=1)
+            {
+                s++;
+            }
+        }
+        System.out.println(s);
+    }
 }
-  }
