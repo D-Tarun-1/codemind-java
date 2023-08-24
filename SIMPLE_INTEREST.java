81501657a0148cbf9e93e5e91139e372af49a3c9
D-Tarun-1 <119721564+D-Tarun-1@users.noreply.github.com>
2023-08-24 23:17:11 +0530

    import java.util.*; public class sample {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int p,t,r,a;         p=sc.nextInt();         t=sc.nextInt();         r=sc.nextInt();         a=(p*t*r)/100;         System.out.println(a);     } }

--- SIMPLE_INTEREST.java
+++ SIMPLE_INTEREST.java
@@ -1,13 +1,14 @@
 import java.util.*;
-public class simple
+public class sample
 {
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int p=sc.nextInt();
-        int t=sc.nextInt();
-        int r=sc.nextInt();
-        int si=(p*t*r)/100;
-        System.out.format("%d ",si);
+        int p,t,r,a;
+        p=sc.nextInt();
+        t=sc.nextInt();
+        r=sc.nextInt();
+        a=(p*t*r)/100;
+        System.out.println(a);
     }
 }
